1cfd03b82692124eaf40fee89f8c6bd1d1ec3821
Albina1993 <alba.tleubergen@gmail.com>
2020-08-07 17:49:10 -0400

    adds more changes

--- recipes/views.py
+++ recipes/views.py
@@ -12,14 +12,16 @@ def index(request):
 
 def details(request, recipe_id):
     try:
-        recipe = Recipe.objects.filter(id=recipe_id).first()
-
+        recipe = Recipe.objects.get(id=recipe_id)
     except Recipe.DoesNotExist:
         raise(Http404)
     return render(request, 'details.html', {'recipe': recipe})
 
 
 def author(request, author_id):
-    author = Author.objects.filter(id=author_id).first()
-    recipes = Recipe.objects.filter(author=author_id)
+    try:
+        author = Author.objects.get(id=author_id)
+        recipes = Recipe.objects.filter(author=author_id)
+    except Author.DoesNotExist:
+        raise(Http404)
     return render(request, 'author.html', {'author': author, 'recipes': recipes})
